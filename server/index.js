8a83c34e2732c779882996c60e7fad457ebe6e91
David Boström <david.bostrom@medieinstitutet.se>
2024-03-13 14:25:44 +0100

    add express and env, basic req/res

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log("Server running");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use("/favorites", (req, res, next) => {
+	// Validate user
+	console.log('User validation!');
+	next();
+});
+
+app.get("/favorites", (req, res) => {
+	// Send favorites for specific user
+	res.status(200).json("These are your favorites!");
+});
+
+app.post("/favorites", (req, res) => {
+	express.json(); // parse request body (is undefined otherwise)
+	// Save to user's favorites
+	res.status(200).json("Favorites saved!");
+});
+
+app.listen(port, () => console.log("Server running!"));
