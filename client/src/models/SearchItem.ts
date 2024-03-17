db9cfb78134e557cb607e79afa5adbf4f8ef2c94
David Boström <david.bostrom@medieinstitutet.se>
2024-03-17 11:43:19 +0100

    add all possible parameters to search models

--- client/src/models/SearchItem.ts
+++ client/src/models/SearchItem.ts
@@ -1,6 +1,22 @@
 interface SearchItem {
-  title: string;
+  displayLink: string;
+  fileFormat: string;
+  htmlSnippet: string;
+  htmlTitle: string;
+  image: {
+    byteSize: number;
+    contextLink: string;
+    height: number;
+    thumbnailHeight: number;
+    thumbnailLink: string;
+    thumbnailWidth: number;
+    width: number;
+  }
+  kind: string;
   link: string;
+  mime: string;
+  snippet: string;
+  title: string;
 }
 
 export default SearchItem;
--- client/src/models/SearchResponse.ts
+++ client/src/models/SearchResponse.ts
@@ -1,17 +1,150 @@
 import SearchItem from "./SearchItem";
 
 interface SearchResponse {
+	context: {
+		title: string;
+	}
 	items: SearchItem[];
+	kind: string;
+	queries: Queries;
 	searchInformation: {
 		formattedSearchTime: string;
 		formattedTotalResults: string;
 		searchTime: number;
 		totalResults: string;
-	  };
-	spelling: {
+	};
+  spelling?: {
 		correctedQuery: string;
 		htmlCorrectedQuery: string;
 	};
+	url: {
+    template: string;
+    type: string;
+  }
+}
+
+interface Queries {
+  previousPage?: [
+    {
+      title: string;
+      totalResults: string;
+      searchTerms: string;
+      count: number;
+      startIndex: number;
+      startPage: number;
+      language: string;
+      inputEncoding: string;
+      outputEncoding: string;
+      safe: string;
+      cx: string;
+      sort: string;
+      filter: string;
+      gl: string;
+      cr: string;
+      googleHost: string;
+      disableCnTwTranslation: string;
+      hq: string;
+      hl: string;
+      siteSearch: string;
+      siteSearchFilter: string;
+      exactTerms: string;
+      excludeTerms: string;
+      linkSite: string;
+      orTerms: string;
+      relatedSite: string;
+      dateRestrict: string;
+      lowRange: string;
+      highRange: string;
+      fileType: string;
+      rights: string;
+      searchType: string;
+      imgSize: string;
+      imgType: string;
+      imgColorType: string;
+      imgDominantColor: string
+    }
+  ];
+  request: [
+    {
+      title: string;
+      totalResults: string;
+      searchTerms: string;
+      count: number;
+      startIndex: number;
+      startPage: number;
+      language: string;
+      inputEncoding: string;
+      outputEncoding: string;
+      safe: string;
+      cx: string;
+      sort: string;
+      filter: string;
+      gl: string;
+      cr: string;
+      googleHost: string;
+      disableCnTwTranslation: string;
+      hq: string;
+      hl: string;
+      siteSearch: string;
+      siteSearchFilter: string;
+      exactTerms: string;
+      excludeTerms: string;
+      linkSite: string;
+      orTerms: string;
+      relatedSite: string;
+      dateRestrict: string;
+      lowRange: string;
+      highRange: string;
+      fileType: string;
+      rights: string;
+      searchType: string;
+      imgSize: string;
+      imgType: string;
+      imgColorType: string;
+      imgDominantColor: string
+    }
+  ];
+  nextPage?: [
+    {
+      title: string;
+      totalResults: string;
+      searchTerms: string;
+      count: number;
+      startIndex: number;
+      startPage: number;
+      language: string;
+      inputEncoding: string;
+      outputEncoding: string;
+      safe: string;
+      cx: string;
+      sort: string;
+      filter: string;
+      gl: string;
+      cr: string;
+      googleHost: string;
+      disableCnTwTranslation: string;
+      hq: string;
+      hl: string;
+      siteSearch: string;
+      siteSearchFilter: string;
+      exactTerms: string;
+      excludeTerms: string;
+      linkSite: string;
+      orTerms: string;
+      relatedSite: string;
+      dateRestrict: string;
+      lowRange: string;
+      highRange: string;
+      fileType: string;
+      rights: string;
+      searchType: string;
+      imgSize: string;
+      imgType: string;
+      imgColorType: string;
+      imgDominantColor: string
+    }
+  ]
 }
 
 export default SearchResponse;
+
