4e720df0848abdbba0cf878e50b703180eb9da84
David Boström <david.bostrom@medieinstitutet.se>
2024-03-20 11:37:16 +0100

    add missing Content-Type header

--- client/src/services/favoritesService.ts
+++ client/src/services/favoritesService.ts
@@ -14,7 +14,7 @@ export const getFavorites = async (user: string) => {
 
 export const postFavorite = async (user: string, favorite: Favorite) => {
   const url = baseUrl + "/favorites/add";
-  const headers = { "user-id": user };
+  const headers = { "Content-Type": "application/json", "user-id": user };
   const body = JSON.stringify(favorite);
 
   const data = await fetchData<string>("POST", url, headers, body);
@@ -23,7 +23,7 @@ export const postFavorite = async (user: string, favorite: Favorite) => {
 
 export const deleteFavorite = async (user: string, imageUrl: Url) => {
   const url = baseUrl + "/favorites/remove";
-  const headers = { "user-id": user };
+  const headers = { "Content-Type": "application/json", "user-id": user };
   const body = JSON.stringify(imageUrl);
 
   const data = await fetchData<string>("DELETE", url, headers, body);
